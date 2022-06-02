d851899384ab034e070916cd93e7deb05712b246
Thivi <theviyanthan@outlook.com>
2022-06-02 23:53:48 +0530

    Add experiments model

---
+++ src/model/experiment.ts
@@ -0,0 +1,24 @@
+import { BYZANTINE_SWARM_STYLE, CONSENSUS_ALGORITHM, DECISION_RULE } from "../constants";
+
+export interface Experiment {
+    decisionRule: DECISION_RULE;
+    percentageOfBlackTiles: number;
+    consensusAlgorithm: string;
+    useClassicalApproach: boolean;
+    numberOfByzantineRobots: number;
+    byzantineSwarmStyle: BYZANTINE_SWARM_STYLE;
+    numberOfRobots: number;
+}
+
+export interface ExperimentData {
+    secondsTaken: number;
+    numberOfWhites: number;
+    numberOfBlacks: number;
+    numberOfRobots: number;
+    isClassical: boolean;
+    consensusAlgorithm: CONSENSUS_ALGORITHM;
+    percentageOfBlackTiles: number;
+    decisionRule: DECISION_RULE;
+    byzantineSwarmStyle: BYZANTINE_SWARM_STYLE;
+    numberOfByzantineRobots: number;
+}
