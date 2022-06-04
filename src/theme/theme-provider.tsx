763d7be899da7b135eab8671ddc9ab60232f0232
Thivi <theviyanthan@outlook.com>
2022-06-05 03:37:15 +0530

    Add theme provider

---
+++ src/theme/theme-provider.tsx
@@ -0,0 +1,40 @@
+import { createTheme, PaletteMode, useMediaQuery } from "@mui/material";
+import { teal } from "@mui/material/colors";
+import { ThemeProvider } from "@mui/system";
+import React, { useEffect } from "react";
+import { PropsWithChildren, ReactElement, useMemo, useState } from "react";
+
+export const ThemeContext = React.createContext<[ mode: PaletteMode, toggleMode: () => void ]>([ "light", () => { } ]);
+
+export const AppThemeProvider = (props: PropsWithChildren<any>): ReactElement => {
+    const { children } = props;
+
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+    const [ mode, setMode ] = useState<PaletteMode>("light");
+
+    useEffect(() => {
+        setMode(prefersDarkMode ? "dark" : "light");
+    }, [ prefersDarkMode ]);
+
+    const theme = useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode: mode,
+                    primary: teal
+                }
+            }),
+        [ mode ]
+    );
+
+    const toggleMode = (): void => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+    };
+
+    return (
+        <ThemeContext.Provider value={ [ mode, toggleMode ] }>
+            <ThemeProvider theme={ theme }>{ children }</ThemeProvider>
+        </ThemeContext.Provider>
+    );
+};
