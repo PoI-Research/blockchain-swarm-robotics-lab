dd7a2c673e7a5e579e22093002b1cda59e985179
Thivi <theviyanthan@outlook.com>
2022-06-05 03:37:31 +0530

    Style app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Box, Grid, Paper } from "@mui/material";
 import React, { FunctionComponent, ReactElement, useEffect, useState } from "react";
 import { clearQueue, deleteExperiment, getQueue } from "./apis";
-import { ExperimentDataView, ExperimentForm, Queue } from "./components";
+import { AppTopBar, ExperimentDataView, ExperimentForm, Queue } from "./components";
 import { Experiment } from "./model/experiment";
+import { AppThemeProvider } from "./theme";
 
 export const App: FunctionComponent = (): ReactElement => {
-    const [queue, setQueue] = useState<Experiment[]>([]);
+    const [ queue, setQueue ] = useState<Experiment[]>([]);
 
     useEffect(() => {
         getQueue().then((response: Experiment[]) => setQueue(response));
@@ -16,17 +18,65 @@ export const App: FunctionComponent = (): ReactElement => {
 
     const clearExperiments = (): void => {
         clearQueue().then(() => getQueue().then((response: Experiment[]) => setQueue(response)));
-    }
+    };
 
     const deleteAnExperiment = (id: number): void => {
         deleteExperiment(id).then(() => getQueue().then((response: Experiment[]) => setQueue(response)));
-    }
+    };
 
     return (
-        <>
-            <ExperimentForm addToQueue={ addToQueue } />
-            <Queue queue={ queue } clearQueue={ clearExperiments } deleteAnExperiment={ deleteAnExperiment } />
-            <ExperimentDataView />
-        </>
+        <AppThemeProvider>
+            <Paper variant="outlined" square sx={ { border: "none", minHeight: "100vmin", height: "100%" } }>
+                <Box sx={ { flexGrow: 1 } }>
+                    <AppTopBar />
+                </Box>
+                <Box sx={ { minHeight: "100vmin" } }>
+                    <Grid container spacing={ 2 } sx={ { minHeight: "100vmin" } }>
+                        <Grid item xs={ 2 }>
+                            <Box
+                                sx={ {
+                                    padding: "1em",
+                                    height: "100%",
+                                    borderRight: 1,
+                                    borderColor: "divider",
+                                } }
+                            >
+                                <ExperimentForm addToQueue={ addToQueue } />
+                            </Box>
+                        </Grid>
+                        <Grid item xs={ 3 }>
+                            <Box
+                                sx={ {
+                                    paddingTop: "1em",
+                                    height: "100%",
+                                    borderRight: 1,
+                                    borderColor: "divider",
+                                    paddingRight: "1em"
+                                } }
+                            >
+                                <Queue
+                                    queue={ queue }
+                                    clearQueue={ clearExperiments }
+                                    deleteAnExperiment={ deleteAnExperiment }
+                                />
+                            </Box>
+                        </Grid>
+                        <Grid item xs={ 7 }>
+                            <Box
+                                sx={ {
+                                    paddingTop: "1em",
+                                    height: "100%",
+                                    borderRight: 1,
+                                    borderColor: "divider",
+                                    paddingRight: "1em"
+                                } }
+                            >
+                                <ExperimentDataView />
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Paper>
+        </AppThemeProvider>
     );
 };
