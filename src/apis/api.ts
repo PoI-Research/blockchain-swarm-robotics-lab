5eff8592a49d2b2f381120c95859dee5a2294cd6
Thivi <theviyanthan@outlook.com>
2022-06-04 22:06:52 +0530

    Add all apis

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -8,12 +8,50 @@ export const getQueue = (): Promise<Experiment[]> => {
 
         return Promise.resolve(queue);
     });
-}
+};
 
 export const getExperimentData = (): Promise<ExperimentData[]> => {
     return fetch(`${ endpoint }/experiment-data`).then(async (response: Response) => {
         const experimentData = await response.json();
 
         return Promise.resolve(experimentData);
+    });
+};
+
+export const addExperiment = (experiment: Experiment): Promise<void> => {
+    const config: RequestInit = {
+        method: "POST",
+        body: JSON.stringify(experiment),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+
+    return fetch(`${ endpoint }/queue`, config).then(async (response: Response) => {
+        return Promise.resolve();
+    });
+};
+
+export const clearQueue = (): Promise<void> => {
+    return fetch(`${ endpoint }/queue`, { method: "DELETE" }).then(async (response: Response) => {
+        return Promise.resolve();
+    });
+};
+
+export const deleteExperiment = (id: number): Promise<void> => {
+    return fetch(`${ endpoint }/queue/${ id }`, { method: "DELETE" }).then(async (response: Response) => {
+        return Promise.resolve();
+    });
+};
+
+export const clearExperimentData = (): Promise<void> => {
+    return fetch(`${ endpoint }/experiment-data`, { method: "DELETE" }).then(async (response: Response) => {
+        return Promise.resolve();
+    });
+};
+
+export const deleteExperimentData = (id: number): Promise<void> => {
+    return fetch(`${ endpoint }/experiment-data/${ id }`, { method: "DELETE" }).then(async (response: Response) => {
+        return Promise.resolve();
     });
 };
