e9164329878150a55943cd5c2e86b01e5959051f
Thivi <theviyanthan@outlook.com>
2022-06-04 22:07:09 +0530

    Format code

--- src/components/ experiment-form.tsx
+++ src/components/ experiment-form.tsx
@@ -1,20 +1,21 @@
 import { FunctionComponent, ReactElement, useState } from "react";
+import { addExperiment, getQueue } from "../apis";
 import { BYZANTINE_SWARM_STYLE, CONSENSUS_ALGORITHM, DECISION_RULE } from "../constants";
 import { Experiment } from "../model";
 
 interface ExperimentFormProps {
-    addToQueue: (experiment: Experiment) => void;
+    addToQueue: (experiments: Experiment[]) => void;
 }
 export const ExperimentForm: FunctionComponent<ExperimentFormProps> = (props: ExperimentFormProps): ReactElement => {
     const { addToQueue } = props;
 
-    const [decisionRule, setDecisionRule] = useState<DECISION_RULE>(0);
-    const [percentageOfBlackTiles, setPercentageOfBlackTiles] = useState<number>(80);
-    const [consensusAlgorithm, setConsensusAlgorithm] = useState<CONSENSUS_ALGORITHM>(CONSENSUS_ALGORITHM.POW);
-    const [useClassicalApproach, setUseClassicalApproach] = useState<boolean>(false);
-    const [numberOfByzantineRobots, setNumberOfByzantineRobots] = useState<number>(0);
-    const [byzantineSwarmStyle, setByzantineSwarmStyle] = useState<BYZANTINE_SWARM_STYLE>(0);
-    const [numberOfRobots, setNumberOfRobots] = useState<number>(0);
+    const [ decisionRule, setDecisionRule ] = useState<DECISION_RULE>(0);
+    const [ percentageOfBlackTiles, setPercentageOfBlackTiles ] = useState<number>(80);
+    const [ consensusAlgorithm, setConsensusAlgorithm ] = useState<CONSENSUS_ALGORITHM>(CONSENSUS_ALGORITHM.POW);
+    const [ useClassicalApproach, setUseClassicalApproach ] = useState<boolean>(false);
+    const [ numberOfByzantineRobots, setNumberOfByzantineRobots ] = useState<number>(0);
+    const [ byzantineSwarmStyle, setByzantineSwarmStyle ] = useState<BYZANTINE_SWARM_STYLE>(0);
+    const [ numberOfRobots, setNumberOfRobots ] = useState<number>(0);
 
     const handleSubmit = (): void => {
         const experiment: Experiment = {
@@ -26,37 +27,36 @@ export const ExperimentForm: FunctionComponent<ExperimentFormProps> = (props: Ex
             byzantineSwarmStyle,
             numberOfRobots
         };
-
-        addToQueue(experiment);
+        addExperiment(experiment).then(() => getQueue().then((response: Experiment[]) => addToQueue(response)));
     };
 
     return (
         <div>
-            <input type="number" value={decisionRule} onChange={(e) => setDecisionRule(parseInt(e.target.value))} />
+            <input type="number" value={ decisionRule } onChange={ (e) => setDecisionRule(parseInt(e.target.value)) } />
             <input
                 type="number"
-                value={percentageOfBlackTiles}
-                onChange={(e) => setPercentageOfBlackTiles(parseInt(e.target.value))}
+                value={ percentageOfBlackTiles }
+                onChange={ (e) => setPercentageOfBlackTiles(parseInt(e.target.value)) }
             />
             <input
                 type="string"
-                value={consensusAlgorithm}
-                onChange={(e) => setConsensusAlgorithm(e.target.value as CONSENSUS_ALGORITHM)}
+                value={ consensusAlgorithm }
+                onChange={ (e) => setConsensusAlgorithm(e.target.value as CONSENSUS_ALGORITHM) }
             />
             <input
                 type="checkbox"
-                checked={useClassicalApproach}
-                onChange={(e) => setUseClassicalApproach(e.target.checked)}
+                checked={ useClassicalApproach }
+                onChange={ (e) => setUseClassicalApproach(e.target.checked) }
             />
             <input
                 type="number"
-                value={numberOfByzantineRobots}
-                onChange={(e) => setNumberOfByzantineRobots(parseInt(e.target.value))}
+                value={ numberOfByzantineRobots }
+                onChange={ (e) => setNumberOfByzantineRobots(parseInt(e.target.value)) }
             />
             <input
                 type="number"
-                value={byzantineSwarmStyle}
-                onChange={(e) => setByzantineSwarmStyle(parseInt(e.target.value))}
+                value={ byzantineSwarmStyle }
+                onChange={ (e) => setByzantineSwarmStyle(parseInt(e.target.value)) }
             />
             <input type="number" value={ numberOfRobots } onChange={ (e) => setNumberOfRobots(parseInt(e.target.value)) } />
             <input type="button" value="Submit" onClick={ handleSubmit } />
