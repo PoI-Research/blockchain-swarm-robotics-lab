af190a5ae0473a3995bddcf57731051f0739b36a
Thivi <theviyanthan@outlook.com>
2022-06-04 22:06:44 +0530

    Add ckear and delete methods

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
-import React, { FunctionComponent, ReactElement, useState } from "react";
+import React, { FunctionComponent, ReactElement, useEffect, useState } from "react";
+import { clearQueue, deleteExperiment, getQueue } from "./apis";
 import { ExperimentDataView, ExperimentForm, Queue } from "./components";
 import { Experiment } from "./model/experiment";
 
 export const App: FunctionComponent = (): ReactElement => {
     const [queue, setQueue] = useState<Experiment[]>([]);
 
-    const addToQueue = (experiment: Experiment) => {
-        setQueue((queue) => {
-            const newQueue = [ ...queue, experiment ];
+    useEffect(() => {
+        getQueue().then((response: Experiment[]) => setQueue(response));
+    }, []);
 
-            return newQueue;
-        });
+    const addToQueue = (experiments: Experiment[]): void => {
+        setQueue(experiments);
     };
+
+    const clearExperiments = (): void => {
+        clearQueue().then(() => getQueue().then((response: Experiment[]) => setQueue(response)));
+    }
+
+    const deleteAnExperiment = (id: number): void => {
+        deleteExperiment(id).then(() => getQueue().then((response: Experiment[]) => setQueue(response)));
+    }
 
     return (
         <>
-            <ExperimentForm addToQueue={addToQueue} />
-            <Queue queue={ queue } />
+            <ExperimentForm addToQueue={ addToQueue } />
+            <Queue queue={ queue } clearQueue={ clearExperiments } deleteAnExperiment={ deleteAnExperiment } />
             <ExperimentDataView />
         </>
     );
