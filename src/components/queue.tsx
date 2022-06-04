4dfb06a1e167f224e6102ba8f5fb1e1e9ce657cd
Thivi <theviyanthan@outlook.com>
2022-06-05 03:38:22 +0530

    Style queue

--- src/components/queue.tsx
+++ src/components/queue.tsx
@@ -1,5 +1,27 @@
+import {
+    BugReport,
+    Circle,
+    CircleOutlined,
+    ContrastOutlined,
+    DeleteForever,
+    GridView,
+    PlayArrow,
+    SmartToy
+} from "@mui/icons-material";
+import {
+    Box,
+    Button,
+    Chip,
+    CircularProgress,
+    IconButton,
+    List,
+    ListItem,
+    Paper,
+    TextField,
+    Typography
+} from "@mui/material";
 import { FunctionComponent, ReactElement, useState } from "react";
-import { RUN_EXPERIMENTS } from "../constants";
+import { BYZANTINE_SWARM_STYLE, CONSENSUS_ALGORITHM, DECISION_RULE, RUN_EXPERIMENTS } from "../constants";
 import { Experiment } from "../model";
 import { socket } from "../utils";
 
@@ -17,27 +39,133 @@ export const Queue: FunctionComponent<QueueProps> = (props: QueueProps): ReactEl
         socket.emit(RUN_EXPERIMENTS, repetitions);
     };
 
+    const getDecisionRule = (rule: DECISION_RULE): string => {
+        switch (rule) {
+            case DECISION_RULE.DC:
+                return "DC";
+            case DECISION_RULE.DMMD:
+                return "DMMD";
+            case DECISION_RULE.DMVD:
+                return "DMVD";
+            default:
+                return "";
+        }
+    };
+
+    const formatConsensusAlgorithm = (algorithm: CONSENSUS_ALGORITHM): string => {
+        if (algorithm === CONSENSUS_ALGORITHM.POW) {
+            return "PoW";
+        }
+
+        return "PoI";
+    };
+
+    const generateByzantineSwarmStyleICon = (style: BYZANTINE_SWARM_STYLE): ReactElement | null => {
+        switch (style) {
+            case BYZANTINE_SWARM_STYLE.BLACK_BYZANTINE_ROBOTS:
+                return (
+                    <Box sx={ { color: "black" } }>
+                        <Circle color="inherit" />
+                    </Box>
+                );
+            case BYZANTINE_SWARM_STYLE.WHITE_BYZANTINE_ROBOTS:
+                return (
+                    <Box>
+                        <CircleOutlined />
+                    </Box>
+                );
+            case BYZANTINE_SWARM_STYLE.BLACK_WHITE_BYZANTINE_ROBOTS:
+                return (
+                    <Box>
+                        <ContrastOutlined />
+                    </Box>
+                );
+        }
+
+        return null;
+    };
+
     const generateQueueElement = (experiment: Experiment, index: number): ReactElement => {
         return (
-            <div key={ index }>
-                <span>{ experiment?.decisionRule }</span>
-                <span>{ experiment?.byzantineSwarmStyle }</span>
-                <span>{ experiment?.consensusAlgorithm }</span>
-                <span>{ experiment?.useClassicalApproach }</span>
-                <span>{ experiment?.percentageOfBlackTiles }</span>
-                <span>{ experiment?.numberOfByzantineRobots }</span>
-                <span>{ experiment?.numberOfRobots }</span>
-                <input type="button" value="Delete" onClick={ () => deleteAnExperiment(index) } />
-            </div>
+            <ListItem key={ index }>
+                <Paper
+                    variant="outlined"
+                    sx={ {
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-between",
+                        width: "100%",
+                        padding: "1em"
+                    } }
+                >
+                    <span>
+                        <Chip icon={ <SmartToy /> } label={ experiment?.numberOfRobots } />
+                    </span>
+                    <span>
+                        <Box sx={ { color: "black", position: "relative", display: "inline-flex" } }>
+                            <CircularProgress
+                                color="inherit"
+                                variant="determinate"
+                                value={ experiment?.percentageOfBlackTiles }
+                            />
+                            <Box
+                                sx={ {
+                                    top: 0,
+                                    left: 0,
+                                    bottom: 0,
+                                    right: 0,
+                                    position: "absolute",
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "center"
+                                } }
+                            >
+                                <Typography variant="caption" component="div" color="text.secondary">{ `${ Math.round(
+                                    experiment?.percentageOfBlackTiles
+                                ) }%` }</Typography>
+                            </Box>
+                        </Box>
+                    </span>
+                    <span>
+                        <Chip variant="outlined" label={ getDecisionRule(experiment?.decisionRule) } />
+                    </span>
+                    <span>{ !experiment?.useClassicalApproach && <GridView /> }</span>
+                    { !experiment?.useClassicalApproach && (
+                        <span>
+                            <Chip color="primary" label={ formatConsensusAlgorithm(experiment?.consensusAlgorithm) } />
+                        </span>
+                    ) }
+                    <span>
+                        <Chip icon={ <BugReport /> } label={ experiment?.numberOfByzantineRobots } />
+                    </span>
+                    { experiment?.numberOfByzantineRobots > 0 && (
+                        <span>{ generateByzantineSwarmStyleICon(experiment?.byzantineSwarmStyle) }</span>
+                    ) }
+                    <IconButton onClick={ () => deleteAnExperiment(index) }>
+                        <DeleteForever />
+                    </IconButton>
+                </Paper>
+            </ListItem>
         );
     };
 
     return (
-        <div>
-            <input type="button" value="Clear Queue" onClick={ clearQueue } />
-            { queue?.map((experiment: Experiment, index: number) => generateQueueElement(experiment, index)) }
-            <input type="number" value={ repetitions } onChange={ (e) => setRepetitions(parseInt(e.target.value)) } />
-            <input type="button" value="Run Experiment" onClick={ runExperiment } />
-        </div>
+        <Box>
+            <Button startIcon={ <DeleteForever /> } onClick={ clearQueue }>
+                Clear Queue
+            </Button>
+            <List>
+                { queue?.map((experiment: Experiment, index: number) => generateQueueElement(experiment, index)) }
+            </List>
+            <TextField
+                type="number"
+                value={ repetitions }
+                onChange={ (e) => setRepetitions(parseInt(e.target.value)) }
+                label="Repetitions"
+            />
+            <Button variant="contained" startIcon={ <PlayArrow /> } onClick={ runExperiment }>
+                Run Experiments
+            </Button>
+        </Box>
     );
 };
