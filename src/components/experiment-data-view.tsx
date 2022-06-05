032d0eba5dfc24dcaf2bfe23bf76cf0901d5a7ba
Thivi <theviyanthan@outlook.com>
2022-06-05 18:30:01 +0530

    Style experiment data view

--- src/components/experiment-data-view.tsx
+++ src/components/experiment-data-view.tsx
@@ -1,12 +1,50 @@
-import { FunctionComponent, ReactElement, useEffect, useRef, useState } from "react";
+import { DeleteForever } from "@mui/icons-material";
+import {
+    Box,
+    Button,
+    Divider,
+    IconButton,
+    LinearProgress,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Typography
+} from "@mui/material";
+import { FunctionComponent, ReactElement, useEffect, useMemo, useRef, useState } from "react";
 import { clearExperimentData, deleteExperimentData, getExperimentData } from "../apis";
-import { EXPERIMENT_DATA } from "../constants";
-import { ExperimentData } from "../model";
+import { CONSENSUS_ALGORITHM, EXPERIMENT_DATA } from "../constants";
+import { Experiment, ExperimentData } from "../model";
 import { socket } from "../utils";
 
-export const ExperimentDataView: FunctionComponent = (): ReactElement => {
+interface ExperimentDataViewProps {
+    queue: Experiment[];
+    repetitions: number;
+    isRunning: boolean;
+}
+
+export const ExperimentDataView: FunctionComponent<ExperimentDataViewProps> = (
+    props: ExperimentDataViewProps
+): ReactElement => {
+    const { queue, repetitions, isRunning } = props;
+
     const [ liveData, setLiveData ] = useState<ExperimentData[]>([]);
+    const [ receivedDataRows, setReceivedDataRows ] = useState<number>(0);
+
     const init = useRef(true);
+
+    const totalData = useMemo(() => {
+        return repetitions * queue?.length;
+    }, [ repetitions, queue ]);
+
+    useEffect(() => {
+        if (!isRunning) {
+            setReceivedDataRows(0);
+        }
+    }, [ isRunning ]);
 
     useEffect(() => {
         getExperimentData()
@@ -16,6 +54,7 @@ export const ExperimentDataView: FunctionComponent = (): ReactElement => {
             .finally(() => {
                 if (init.current) {
                     socket.on(EXPERIMENT_DATA, (data: ExperimentData) => {
+                        setReceivedDataRows((rows) => rows + 1);
                         setLiveData((liveData) => {
                             const newData = [ ...liveData, data ];
 
@@ -41,23 +80,67 @@ export const ExperimentDataView: FunctionComponent = (): ReactElement => {
     };
 
     return (
-        <div>
-            <input type="button" value="Clear Data" onClick={ clearData } />
-            { liveData.map((experimentData: ExperimentData, index: number) => (
-                <div key={ index }>
-                    <span>{ experimentData.decisionRule }</span>
-                    <span>{ experimentData.byzantineSwarmStyle }</span>
-                    <span>{ experimentData.consensusAlgorithm }</span>
-                    <span>{ experimentData.isClassical }</span>
-                    <span>{ experimentData.percentageOfBlackTiles }</span>
-                    <span>{ experimentData.numberOfByzantineRobots }</span>
-                    <span>{ experimentData.numberOfRobots }</span>
-                    <span>{ experimentData.secondsTaken }</span>
-                    <span>{ experimentData.numberOfBlacks }</span>
-                    <span>{ experimentData.numberOfWhites }</span>
-                    <input type="button" value="Delete" onClick={ () => deleteExperiment(index) } />
-                </div>
-            )) }
-        </div>
+        <Box>
+            <Box sx={ { display: "flex", justifyContent: "space-between", marginBottom: "1em" } }>
+                <Typography variant="h6">Experiment Data</Typography>
+                <Button startIcon={ <DeleteForever /> } onClick={ clearData }>
+                    Clear Data
+                </Button>
+            </Box>
+            <Divider />
+            { isRunning && (
+                <LinearProgress
+                    variant="buffer"
+                    value={ (receivedDataRows / totalData) * 100 }
+                    valueBuffer={ ((receivedDataRows + 1) / totalData) * 100 }
+                />
+            ) }
+            <TableContainer component={ Paper } sx={ { marginTop: "1em", overflow: "auto", marginBottom: "2em" } }>
+                <Table stickyHeader>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>ID</TableCell>
+                            <TableCell>No. of Robots</TableCell>
+                            <TableCell>% of Black Tiles</TableCell>
+                            <TableCell>Decision Rule</TableCell>
+                            <TableCell>Approach</TableCell>
+                            <TableCell>No. of Byzantine Robots</TableCell>
+                            <TableCell>Byzantine Swarm Style</TableCell>
+                            <TableCell>No. of Black Tiles</TableCell>
+                            <TableCell>No. of White Tiles</TableCell>
+                            <TableCell>Time Taken(s)</TableCell>
+                            <TableCell></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        { liveData.map((experimentData: ExperimentData, index: number) => (
+                            <TableRow key={ index }>
+                                <TableCell>{ experimentData.id }</TableCell>
+                                <TableCell>{ experimentData.numberOfRobots }</TableCell>
+                                <TableCell>{ experimentData.percentageOfBlackTiles }</TableCell>
+                                <TableCell>{ experimentData.decisionRule }</TableCell>
+                                <TableCell>
+                                    { experimentData.isClassical
+                                        ? "Classical"
+                                        : experimentData.consensusAlgorithm === CONSENSUS_ALGORITHM.POI
+                                            ? "PoI"
+                                            : "PoW" }
+                                </TableCell>
+                                <TableCell>{ experimentData.numberOfByzantineRobots }</TableCell>
+                                <TableCell>{ experimentData.byzantineSwarmStyle }</TableCell>
+                                <TableCell>{ experimentData.numberOfBlacks }</TableCell>
+                                <TableCell>{ experimentData.numberOfWhites }</TableCell>
+                                <TableCell>{ experimentData.secondsTaken }</TableCell>
+                                <TableCell>
+                                    <IconButton onClick={ () => deleteExperiment(index) }>
+                                        <DeleteForever />
+                                    </IconButton>
+                                </TableCell>
+                            </TableRow>
+                        )) }
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Box>
     );
 };
